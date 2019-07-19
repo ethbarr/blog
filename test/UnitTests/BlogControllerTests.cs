833a21fa79470fcc398911bc9d22016b6fa3bf80
Ethan Barrett <ethan_barrett@live.com>
2019-07-19 12:27:10 -0400

    add tests

---
+++ test/UnitTests/BlogControllerTests.cs
@@ -0,0 +1,10 @@
+using Xunit;
+
+namespace UnitTests {
+    public class BlogControllerTests{
+        [Fact]
+        public void CanAssertTrue() {
+            Assert.True(true, "My first test");
+        }
+    }
+}
